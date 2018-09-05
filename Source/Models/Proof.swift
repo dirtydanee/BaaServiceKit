9e2d2c59f653f6e093584cae0feccd1ca5b8c12f
David Szurma <szurmadavid@gmail.com>
2018-09-05 21:06:00 +0200

    remove optionality from MetaData

--- Source/Models/Proof.swift
+++ Source/Models/Proof.swift
@@ -10,18 +10,18 @@ public struct Proof {
     
     let nodeHash: NodeHash
     let status: Status
-    let metadata: [String: Any]?
+    let metadata: [String: Any]
     
     static func make(from chainPointProof: ChainpointProofResponse, with nodeHash: NodeHash) -> Proof {
         
-        var metadata: [String: Any]?
+        var metadata = [String: Any]()
         
         do {
             let jsonData = try JSONEncoder().encode(chainPointProof)
             let decoded = try JSONSerialization.jsonObject(with: jsonData, options: [])
             
-            if let dictFromJSON = decoded as? [String: Any] {
-                metadata = dictFromJSON
+            if let dict = decoded as? [String: Any] {
+                metadata = dict
             }
         } catch {
             print("Proof.make -> json error: \(error)")
